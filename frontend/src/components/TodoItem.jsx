b1a35001fb43e9ba400eea5f080665cbe98f489c
kimhaein <rlagpdls93@gmail.com>
2020-04-26 19:18:04 +0900

    코드 수정

--- frontend/src/components/TodoItem.jsx
+++ frontend/src/components/TodoItem.jsx
@@ -1,37 +1,38 @@
 import React, { useState } from 'react';
 
 function TodoItem({data, deleteTodoList, editTodoList}) {
+
   const [editTodoId, setEditTodoId] = useState(0)
   const [text, setText] = useState('')
   const [isChecked, serIsChecked] = useState((data.is_complete === 'Y') ? true : false)
 
-  const renderTodoText = (todo) => {
-    const tagsList = todo.tag.map((v)=>{
-      const is_complete = todo.completedTags.includes(v)
+  const renderTodoText = () => {
+    const tagsList = data.tag.map((v)=>{
+      const is_complete = data.completedTags.includes(v)
       return (<span key={`tag-${v}`}className={`tagList ${(is_complete)?'complete':''}`}>@{v}</span>)
     })
 
-    if(editTodoId === todo.id){
-      return <input type="text" defaultValue={todo.text} onChange={({target})=>{ setText(target.value)}}/>
+    if(editTodoId === data.id){
+      return <input type="text" defaultValue={data.text} onChange={({target})=>{ setText(target.value)}}/>
     }else {
       return (
         <>
-          <div className="todoText">{todo.text}</div>
-          <div className="todoDate">create | {todo.create_date} {todo.edit_date?`/ edit | ${todo.edit_date}`:''}</div>
+          <div className="todoText">{data.text}</div>
+          <div className="todoDate">create | {data.create_date} {data.edit_date?`/ edit | ${data.edit_date}`:''}</div>
           <div>{tagsList}</div>
         </>
       )
     }
   }
 
-  const renderEditBtn = (todo) => {
-    if(editTodoId === todo.id){
-      return <span className="editBtn" onClick={() => { editTodoList(text, todo.id ,todo.is_complete); setEditTodoId(0);}}>수정완료</span>
+  const renderEditBtn = () => {
+    if(editTodoId === data.id){
+      return <span className="editBtn" onClick={() => { editTodoList(text, data.id ,data.is_complete); setEditTodoId(0);}}>수정완료</span>
     }else {
       return (
         <>
-          <span className="deleteBtn" onClick={() => { deleteTodoList(todo.id) }}>삭제</span>
-          <span className="editBtn" onClick={() => { setEditTodoId(todo.id)}}>수정</span>
+          <span className="deleteBtn" onClick={() => { deleteTodoList(data.id) }}>삭제</span>
+          <span className="editBtn" onClick={() => { setEditTodoId(data.id)}}>수정</span>
         </>
       )
     }
@@ -40,12 +41,12 @@ function TodoItem({data, deleteTodoList, editTodoList}) {
   const isComplete = (target, todoId, completedTags , tag) => {
 
     if(completedTags.length !== tag.length) {
-      alert('참조 리스트를 전부 완료 후, 해당 리스트를 완료 할 수 있습니다.')
       serIsChecked(false)
+      alert('참조 리스트를 전부 완료 후, 해당 리스트를 완료 할 수 있습니다.')
     }else {
       const is_complete = target.checked ? 'Y' : 'N'
-      editTodoList(text, todoId, is_complete)
       serIsChecked(!isChecked)
+      editTodoList(text, todoId, is_complete)
     }
   }
 
@@ -62,10 +63,10 @@ function TodoItem({data, deleteTodoList, editTodoList}) {
         <span className="todoId">{data.id}</span>
       </div>
       <div className="todoTextBox">
-        {renderTodoText(data)}
+        {renderTodoText()}
       </div>
       <div className="btnBox">
-        {renderEditBtn(data)}
+        {renderEditBtn()}
       </div>
       {(isChecked)?<div className="completeBox">Complete</div>:null}
     </div>
